d5a00241425350f54db3ca8ff384bbc47bbdb76e
jhole89 <joel.lutman@gmail.com>
2020-08-02 12:55:56 -0700

    finish wait tests

--- examples/wait/timestamp_path/main.tf
+++ examples/wait/timestamp_path/main.tf
@@ -2,7 +2,7 @@ module "wait_until_t" {
   source      = "../../../modules/wait"
   name        = "wait_until"
   comment     = "An example wait with timestamp path"
-  time_metric = "TimeStampPath"
+  time_metric = "TimestampPath"
   time_value  = "$.expirydate"
 }
 
--- test/wait_test.go
+++ test/wait_test.go
@@ -27,3 +27,69 @@ func TestWaitStateSeconds(t *testing.T) {
 	assert.Equal(t, `{"wait_ten_seconds":{"Comment":"An example wait with seconds","InputPath":null,"Next":"NextState","OutputPath":null,"Seconds":10,"Type":"Wait"}}`, json)
 
 }
+
+func TestWaitStateSecondsPath(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/wait/seconds_path",
+		Upgrade:      true,
+	}
+
+	// Act
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Teardown
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Test
+	json := terraform.Output(t, terraformOptions, "json")
+	assert.Equal(t, `{"wait_until":{"Comment":"An example wait with seconds path","InputPath":null,"Next":"NextState","OutputPath":null,"SecondsPath":"$.seconds","Type":"Wait"}}`, json)
+
+}
+
+func TestWaitStateTimestamp(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/wait/timestamp",
+		Upgrade:      true,
+	}
+
+	// Act
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Teardown
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Test
+	json := terraform.Output(t, terraformOptions, "json")
+	assert.Equal(t, `{"wait_until":{"Comment":"An example wait with timestamp","InputPath":null,"Next":"NextState","OutputPath":null,"Timestamp":"2016-03-14T01:59:00Z","Type":"Wait"}}`, json)
+
+}
+
+func TestWaitStateTimestampPath(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/wait/timestamp_path",
+		Upgrade:      true,
+	}
+
+	// Act
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Teardown
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Test
+	json := terraform.Output(t, terraformOptions, "json")
+	assert.Equal(t, `{"wait_until":{"Comment":"An example wait with timestamp path","InputPath":null,"Next":"NextState","OutputPath":null,"TimestampPath":"$.expirydate","Type":"Wait"}}`, json)
+
+}
